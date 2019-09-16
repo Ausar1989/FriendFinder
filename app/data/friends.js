28da64bc3982446261be990bd762d3e1f265c1aa
Jermaine Lewis <lewisjermaine22@gmail.com>
2019-09-15 23:53:55 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+
+
+
+var friends =[
+    {
+       name: "Jermaine Lewis",
+       photo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/29510888_2146744948686696_1676755480499860534_n.jpg?_nc_cat=104&_nc_oc=AQkcUk69y4LvEY2d-h71i3XEqup563mzgjpZl7B0yrDfS2kLG_Xz5KfDWNvQ8NEEt6gjmn1d08eo-NSFztLiNGZD&_nc_ht=scontent-dfw5-2.xx&oh=17cb749246be1107946c0680612450e9&oe=5E386178",
+       scores: [5, 4, 3, 3, 4, 4, 3, 3, 4, 5]
+    },
+    {
+        name: "Kanye West",
+        photo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/20031617_1969194833105937_1076282908393452094_n.jpg?_nc_cat=110&_nc_oc=AQlyJ9lnFTnYBTR6XCUkBk9eEBpt-QqttQu4RfonNgCzmfNmt8HT_nUNabf_DdJn9MkpO-MSi--JEfQxCnw8rEsd&_nc_ht=scontent-dfw5-2.xx&oh=e79293f4dd7ff794d39670294118c1ab&oe=5DF265F6",
+        scores:[5, 4, 5, 4, 5, 4, 5, 4, 5, 4]
+    },
+    {
+        name: "Beyonce Knowles",
+        photo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/66467120_10162244854770601_4639875954670305280_n.jpg?_nc_cat=1&_nc_oc=AQkH286v-fENwG0-OVESnlRnYQ9rtfaU4p2cSfrACUXQmR_bcKs_IAZS-4WJZHDIWT4Qwu806OnSl6EMs-i9xi4I&_nc_ht=scontent-dfw5-2.xx&oh=b338fa8a71aed389ce22cd723b0ab5f2&oe=5E38B347",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Yui Aragaki",
+        photo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/1929596_9115637033_9307_n.jpg?_nc_cat=102&_nc_oc=AQnV1QL91rwwC0qZoIT3lquHg3Lalgsb5D1v542k9vydIcqETGEAOLgQTGRPH9vhgCpN17YBF-BZgRxJEMvhEiZF&_nc_ht=scontent-dfw5-2.xx&oh=5d74f3ab7a6a205bf4aaa1e589574869&oe=5DF84BFB",
+        scores: [5, 5, 5, 4, 3, 4, 4, 5, 5, 4, 5]
+    }
+];
+
+module.exports = friends;
